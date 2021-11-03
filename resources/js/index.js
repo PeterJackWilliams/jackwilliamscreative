ecf689ca4df6f29ff8f971cc5d9a4927f65d5cb0
Jack <peter.jack.williams@gmail.com>
2021-11-03 10:42:55 +0000

    javascript add
    no idea if this will work

--- resources/js/index.js
+++ resources/js/index.js
@@ -6,3 +6,24 @@ imgclick.addEventListener("click", () => {
   jackFirst.classList.toggle("hide")
   jackSecond.classList.toggle("hide")
 })
+
+var i = 0;
+var text = []
+var time = 5000;
+
+text[0] = document.getElementById("text1")
+text[1] = document.getElementById("text2")
+text[2] = document.getElementById("text3")
+
+function changeText() {
+  document.slide.src = text[i]
+
+  if(i < text - 1) {
+    ++i 
+  } else {
+    i = 0
+  }
+
+  setTimeout(changeText(), time)
+}
+    window.onload = changeText
